d8cde0587aae9a5d7791814620cdd4d08f652ebb
Scott Smith <scottksmith95@gmail.com>
2012-04-14 22:06:21 -0700

    Update README

---
+++ CSharp.Readability.Console Example/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using CSharp.Readability.Api;
+using CSharp.Readability.Connect;
+using Spring.Social.OAuth1;
+
+namespace CSharp.Readability.Console_Example
+{
+	class Program
+	{
+		// Register your own Readability app at https://www.readability.com/publishers/api
+
+		// Set your consumer key & secret here
+		private const string ReadabilityApiKey = "ENTER YOUR KEY HERE";
+		private const string ReadabilityApiSecret = "ENTER YOUR SECRET HERE";
+
+		private static void Main(string[] args)
+		{
+			try
+			{
+				var readabilityServiceProvider = new ReadabilityServiceProvider(ReadabilityApiKey, ReadabilityApiSecret);
+
+				/* OAuth 'dance' */
+
+				// Authentication using Out-of-band/PIN Code Authentication
+				Console.Write("Getting request token...");
+				var oauthToken = readabilityServiceProvider.OAuthOperations.FetchRequestTokenAsync("oob", null).Result;
+				Console.WriteLine("Done");
+
+				var authenticateUrl = readabilityServiceProvider.OAuthOperations.BuildAuthorizeUrl(oauthToken.Value, null);
+				Console.WriteLine("Redirect user for authentication: " + authenticateUrl);
+				Process.Start(authenticateUrl);
+				Console.WriteLine("Enter PIN Code from Readability authorization page:");
+				var pinCode = Console.ReadLine();
+
+				Console.Write("Getting access token...");
+				var requestToken = new AuthorizedRequestToken(oauthToken, pinCode);
+				var oauthAccessToken = readabilityServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
+				Console.WriteLine("Done");
+
+				/* API */
+
+				var readability = readabilityServiceProvider.GetApi(oauthAccessToken.Value, oauthAccessToken.Secret);
+
+				readability.UserOperations.GetUserAsync()
+					.ContinueWith(task => Console.WriteLine("Username: " + task.Result.Username));
+
+				readability.BookmarkOperations.GetAllBookmarksAsync(1, 50)
+					.ContinueWith(task => Console.WriteLine("All bookmarks: " + task.Result.Bookmarks.Count));
+
+				readability.BookmarkOperations.GetReadingListBookmarksAsync(1, 50)
+					.ContinueWith(task => Console.WriteLine("Reading list bookmarks: " + task.Result.Bookmarks.Count));
+
+				readability.BookmarkOperations.GetFavoriteBookmarksAsync(1, 50)
+					.ContinueWith(task => Console.WriteLine("Favorite bookmarks: " + task.Result.Bookmarks.Count));
+
+				readability.BookmarkOperations.GetArchivedBookmarksAsync(1, 50)
+					.ContinueWith(task => Console.WriteLine("Archived bookmarks: " + task.Result.Bookmarks.Count));
+			}
+			catch (AggregateException ae)
+			{
+				ae.Handle(ex =>
+				{
+				    if (ex is ReadabilityApiException)
+				    {
+				        Console.WriteLine(ex.Message);
+				        return true;
+				    }
+				    return false;
+				});
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			finally
+			{
+				Console.WriteLine("--- hit <return> to quit ---");
+				Console.ReadLine();
+			}
+		}
+	}
+}
---
+++ CSharp.Readability.Console Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharp.Readability.Console Example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CSharp.Readability.Console Example")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("224b286e-25c5-41d3-93a8-a04205a028f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
