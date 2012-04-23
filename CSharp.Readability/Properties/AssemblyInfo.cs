8d5dd8206c57f5b324d97ad2b16c7a95a54c1014
Scott Smith <scottksmith95@gmail.com>
2012-04-23 10:52:25 -0700

    Updated assembly info

--- CSharp.Readability/Properties/AssemblyInfo.cs
+++ CSharp.Readability/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CSharp.Readability")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# .NET wrapper for the Readability RESTful API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Scott Smith")]
 [assembly: AssemblyProduct("CSharp.Readability")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
